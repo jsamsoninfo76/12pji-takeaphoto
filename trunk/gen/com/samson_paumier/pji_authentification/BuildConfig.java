4d988e09dbeaef4b1a8cead351be26be67902bd3
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@3eedbad3-70e3-1edc-e12c-617fc1e825cd>
2013-02-08 16:40:39 +0000

    reinit

--- trunk/gen/com/samson_paumier/pji_authentification/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.samson_paumier.pji_authentification;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
