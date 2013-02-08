5a4d4402bb5118c50494b48dd05cb3865bbb0ce4
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@3eedbad3-70e3-1edc-e12c-617fc1e825cd>
2013-02-08 15:52:59 +0000

    Initial Import

---
+++ trunk/gen/com/samson_paumier/pji_authentification/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.samson_paumier.pji_authentification;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
