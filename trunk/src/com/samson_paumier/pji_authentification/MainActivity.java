4e71026e7e37ebee599c37155d0ffb53d8a28028
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@3eedbad3-70e3-1edc-e12c-617fc1e825cd>
2013-02-08 16:12:44 +0000

    Ajout edittext

--- trunk/src/com/samson_paumier/pji_authentification/MainActivity.java
+++ trunk/src/com/samson_paumier/pji_authentification/MainActivity.java
@@ -1,8 +1,10 @@
 package com.samson_paumier.pji_authentification;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        EditText editTextUsername = new EditText(this);
+        editTextUsername.setHint(R.string.username);
+        editTextUsername.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+        
+        EditText editTextPassword = new EditText(this);
+        editTextPassword.setHint(R.string.password);
+        editTextPassword.setInputType(InputType.TYPE_TEXT_FLAG_MULTI_LINE);
     }
 
     @Override
