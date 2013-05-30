f4ea536050bf538ca701ccd332d32e859cdb104c
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@3eedbad3-70e3-1edc-e12c-617fc1e825cd>
2013-05-30 22:49:29 +0000

    Finalisation

--- trunk/src/com/takeaphoto/database/Base.java
+++ trunk/src/com/takeaphoto/database/Base.java
@@ -8,16 +8,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class Base extends SQLiteOpenHelper {
 	private static final String TABLE_DEMANDE = "table_demande";
 	private static final String COL_ID = "ID";
-	private static final String COL_LOGIN = "Login";
+	private static final String COL_ID_USER = "ID_User";
 	private static final String COL_LAT = "Lat" ;
 	private static final String COL_LNG = "Lng" ;
 	private static final String COL_DESCRIPTION = "Description";
 	private static final String COL_ETAT = "Etat" ;
 
-	private static final String CREATE_BDD = "CREATE TABLE " + TABLE_DEMANDE + " ("
-	+ COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_LOGIN + " TEXT NOT NULL, "
-	+ COL_LAT + " REAL NOT NULL, " + COL_LNG + " REAL NOT NULL, " + COL_DESCRIPTION 
-	+ " TEXT NOT NULL, " + COL_ETAT + " TEXT NOT NULL);";
+	private static final String TABLE_USER = "table_user";
+	private static final String COL_LOGIN = "Login" ;
+	private static final String COL_PASS = "Pass";
+	
+	private static final String CREATE_BDD = "CREATE TABLE " + TABLE_USER + " (" 
+	+ COL_ID + " INTEGER PRIMARY KEY, " + COL_LOGIN + " TEXT NOT NULL, "
+	+ COL_PASS + " TEXT NOT NULL );";
+	
+	private static final String CREATE_BDD2 = "CREATE TABLE " + TABLE_DEMANDE + " ("
+			+ COL_ID + " INTEGER PRIMARY KEY, " + COL_ID_USER + " INTEGER NOT NULL, "
+			+ COL_LAT + " REAL NOT NULL, " + COL_LNG + " REAL NOT NULL, " + COL_DESCRIPTION 
+			+ " TEXT NOT NULL, " + COL_ETAT + " INTEGER NOT NULL);" ;
  
 	public Base(Context context, String name, CursorFactory factory, int version) {
 		super(context, name, factory, version);
@@ -27,13 +35,13 @@ public class Base extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase db) {
 		//on créé la table à partir de la requête écrite dans la variable CREATE_BDD
 		db.execSQL(CREATE_BDD);
+		db.execSQL(CREATE_BDD2) ;
 	}
  
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		//On peut fait ce qu'on veut ici moi j'ai décidé de supprimer la table et de la recréer
-		//comme ça lorsque je change la version les id repartent de 0
 		db.execSQL("DROP TABLE " + TABLE_DEMANDE + ";");
+		db.execSQL("DROP TABLE " + TABLE_USER + ";");
 		onCreate(db);
 	}
 }
