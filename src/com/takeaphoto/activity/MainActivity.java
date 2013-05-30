6de42f23ebd9b8836e186800e2232dcd056416ae
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@3eedbad3-70e3-1edc-e12c-617fc1e825cd>
2013-05-30 22:49:29 +0000

    Finalisation

--- src/com/takeaphoto/activity/MainActivity.java
+++ src/com/takeaphoto/activity/MainActivity.java
@@ -10,40 +10,24 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.takeaphoto.database.DemandesBDD;
+import com.takeaphoto.utils.CustomViewPager;
 
 public class MainActivity extends FragmentActivity implements
 		ActionBar.TabListener {
 
-	SectionsPagerAdapter mSectionsPagerAdapter;
-	CustomViewPager mViewPager;
-	MapAdd mapAdd = new MapAdd() ;
-	ManagerActivity manager = new ManagerActivity() ;
-	MapReponse mapRep = new MapReponse() ;
+	private SectionsPagerAdapter mSectionsPagerAdapter;
+	private CustomViewPager mViewPager;
+	private ManagerActivity manager = new ManagerActivity() ;
+	private MapAddActivity mapAdd = new MapAddActivity() ;
+	private MapReponseActivity mapRep = new MapReponseActivity() ;
 	final int NB_ONGLET = 3 ;
-	DemandesBDD demandeBdd ;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		demandeBdd = new DemandesBDD(this) ;
-		
-		mapAdd.setDemandeBdd(demandeBdd) ;
-		mapAdd.setMainActivity(this) ;
-		
-		manager.setMainActivity(this) ;
-		manager.setDemandeBdd(demandeBdd) ;
-		
-		mapRep.setDemandeBdd(demandeBdd) ;
-		mapRep.setMainActivity(this) ;
 		
 		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
@@ -51,8 +35,7 @@ public class MainActivity extends FragmentActivity implements
 	
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
+		mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (CustomViewPager) findViewById(R.id.photosViewPager);
@@ -61,37 +44,38 @@ public class MainActivity extends FragmentActivity implements
 		// When swiping between different sections, select the corresponding
 		// tab. We can also use ActionBar.Tab#select() to do this if we have
 		// a reference to the Tab.
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
 					@Override
 					public void onPageSelected(int position) {
 						actionBar.setSelectedNavigationItem(position);
 					}
 				});
-
+		
 		// For each of the sections in the app, add a tab to the action bar.
 		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
 			// Create a tab with text corresponding to the page title defined by
 			// the adapter. Also specify this Activity object, which implements
 			// the TabListener interface, as the callback (listener) for when
 			// this tab is selected.
-			actionBar.addTab(actionBar.newTab()
-					.setText(mSectionsPagerAdapter.getPageTitle(i))
-					.setTabListener(this));
+				actionBar.addTab(actionBar.newTab()
+						.setText(mSectionsPagerAdapter.getPageTitle(i))
+						.setTabListener(this));
 		}
-		
-	
+
 		mViewPager.setPagingEnabled(false) ;
-		
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
+	public void onResume() {
+		super.onResume();
+	}
+	
 	@Override
 	public void onTabSelected(ActionBar.Tab tab,
 			FragmentTransaction fragmentTransaction) {
@@ -101,16 +85,10 @@ public class MainActivity extends FragmentActivity implements
 	}
 
 	@Override
-	public void onTabUnselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
+	public void onTabUnselected(ActionBar.Tab tab,FragmentTransaction fragmentTransaction) {}
 
 	@Override
-	public void onTabReselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	
+	public void onTabReselected(ActionBar.Tab tab,FragmentTransaction fragmentTransaction) {}
 
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -126,7 +104,6 @@ public class MainActivity extends FragmentActivity implements
 		public Fragment getItem(int position) {
 			
 			Fragment frag = new Fragment() ;
-			
 			switch (position) {
 			case 0:
 				frag = mapAdd ;
@@ -138,13 +115,6 @@ public class MainActivity extends FragmentActivity implements
 				
 			case 2:
 				frag = manager ;
-				break;
-				
-			default:
-				frag = new DummySectionFragment();
-				Bundle args = new Bundle();
-				args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-				frag.setArguments(args);
 				break;
 			}
 			
@@ -171,36 +141,4 @@ public class MainActivity extends FragmentActivity implements
 			return null;
 		}
 	}
-	
-	
-
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
-					container, false);
-			
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-		
-	}
-
 }
